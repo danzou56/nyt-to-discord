1f8feae432d06e15f252a90935d0dd87a67f58c7
Dan Zou <danzou193@gmail.com>
2023-05-08 23:43:30 -0400

    Send message to discord

--- nyt_to_discord/main.py
+++ nyt_to_discord/main.py
@@ -1,12 +1,35 @@
 import os
 
+import discord
+
 from nyt_to_discord.nyt.Leaderboard import Leaderboard
 
-NYT_COOKIES_ENV_VAR_NAME = "NYT_COOKIES"
+COOKIES = "NYT_COOKIES"
+CHANNEL_ID = "DISCORD_DTS_CHANNEL_ID"
+# CHANNEL_ID = "DISCORD_BBB_CHANNEL_ID"
+BOT_TOKEN = "DISCORD_BOT_TOKEN"
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+class MSG:
+    msg = "hello"
 
 
 def main():
-    leaderboard = Leaderboard(os.environ[NYT_COOKIES_ENV_VAR_NAME])
+    leaderboard = Leaderboard(os.environ[COOKIES])
+    print(leaderboard.date)
+    MSG.msg = str(leaderboard.scores)
+    client.run(token=os.environ[BOT_TOKEN])
+
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(int(os.environ[CHANNEL_ID]))
+    await channel.send(MSG.msg)
+    print("Sent!")
+    await client.close()
 
 
 if __name__ == "__main__":
