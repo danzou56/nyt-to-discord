c395dbe202174bd184c333a831bb06933dd44173
Dan Zou <danzou193@gmail.com>
2023-05-17 21:10:19 -0400

    Notify on everybody completed

--- nyt_to_discord/bot.py
+++ nyt_to_discord/bot.py
@@ -16,6 +16,7 @@ _log = logging.getLogger(__name__)
 class NytDiscordBot(discord.Client):
     MSG_DATE_FORMAT = "%A, %B %-d, %Y"
     REFRESH_SECONDS = 120
+    NOTIF_ROLE_ID = 1108558393679040643
 
     def __init__(
         self, nyt_cookies: str, channel_id: int, err_channel_id: int, *args, **kwargs
@@ -46,24 +47,17 @@ class NytDiscordBot(discord.Client):
         latest_date = self._db.most_recent_date()
         updated_scores = self._db.update_scores(self.leaderboard.scores)
         mini_date = self.leaderboard.date
-        if mini_date != latest_date:
-            return await self.send_new_leaderboard_message()
-
-        if updated_scores:
-            print("Editing!")
-            return await self.update_leaderboard_message(
-                datetime.datetime.strftime(mini_date, self.MSG_DATE_FORMAT)
-            )
+        if mini_date != latest_date or updated_scores:
+            await self._send_or_update_leaderboard_message()
 
     @refresh_scores.before_loop
     async def _refresh_scores_before(self):
         await self.wait_until_ready()
 
-    async def send_new_leaderboard_message(self) -> None:
-        channel = self.get_channel(self._channel_id)
-        await channel.send(self._build_leaderboard_msg())
+    async def _send_or_update_leaderboard_message(self) -> None:
+        scores = self.leaderboard.scores
+        date = datetime.datetime.strftime(self.leaderboard.date, self.MSG_DATE_FORMAT)
 
-    async def update_leaderboard_message(self, target: str) -> None:
         channel = self.get_channel(self._channel_id)
         messages = channel.history(
             after=datetime.datetime.now() - datetime.timedelta(days=3),
@@ -72,11 +66,16 @@ class NytDiscordBot(discord.Client):
         async for message in messages:
             if message.author.id != self.user.id:
                 continue
-            if target in message.content:
-                await message.edit(content=self._build_leaderboard_msg())
-                return
+            if date in message.content:
+                await message.edit(content=self._build_leaderboard_msg(date, scores))
+                break
+        else:
+            await channel.send(self._build_leaderboard_msg(date, scores))
 
-        raise RuntimeError(f"No message with content {target} found")
+        if all(score.time is not None for score in scores):
+            await channel.send(
+                f"<@{self.NOTIF_ROLE_ID}> Check it out, all the scores are in!"
+            )
 
     async def on_error(self, event_method: str, /, *args: Any, **kwargs: Any) -> None:
         traceback.print_exc()
@@ -93,9 +92,7 @@ class NytDiscordBot(discord.Client):
         finally:
             await self.close()
 
-    def _build_leaderboard_msg(self) -> str:
-        scores = self.leaderboard.scores
-        date = datetime.datetime.strftime(self.leaderboard.date, self.MSG_DATE_FORMAT)
+    def _build_leaderboard_msg(self, date, scores) -> str:
         formatted_table = tabulate(
             [
                 [
